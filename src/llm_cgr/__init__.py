b5915d043158cb69c462f57b14ced1589fc3fea1
Luke Twist <itsluketwist@gmail.com>
2025-04-15 18:32:24 +0100

    Refactor the package structure.

--- src/llm_cgr/__init__.py
+++ src/llm_cgr/__init__.py
@@ -1,7 +1,7 @@
 from llm_cgr import api, md
-from llm_cgr.api.api_utils import get_client, quick_complete
+from llm_cgr.api import get_client, quick_complete
 from llm_cgr.json_utils import read_json, save_json
-from llm_cgr.md.classes import CodeBlock, Markdown
+from llm_cgr.md import CodeBlock, Markdown
 
 
 __all__ = [
